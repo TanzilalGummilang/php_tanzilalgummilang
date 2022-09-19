b4e030e8066d0a9f86881a6f0b39e36b9324c356
TanzilalGummilang <tanzilalgummilang@gmail.com>
2022-09-19 10:44:01 +0700

    update

--- config/soal3_connect-db.php
+++ config/soal3_connect-db.php
@@ -18,20 +18,3 @@ function getData($query)
 
   return $data;
 }
-
-function searchData($query)
-{
-  global $connection;
-  $result = mysqli_query($connection, $query);
-
-  if (!$result) {
-    echo mysqli_error($connection);
-  }
-
-  $data = [];
-  while ($row = mysqli_fetch_assoc($result)) {
-    $data[] = $row;
-  }
-
-  return $data;
-}
